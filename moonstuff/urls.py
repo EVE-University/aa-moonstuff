8257061efb1fb079beee6d03ffe14c8f8979e4e0
colcrunch <rhartnett35@gmail.com>
2021-01-13 16:21:28 -0500

    Add url for the add_character view.

--- moonstuff/urls.py
+++ moonstuff/urls.py
@@ -7,4 +7,5 @@ app_name = "moonstuff"
 urlpatterns = [
     url(r'^$', views.dashboard, name='dashboard'),
     url(r'^scan/$', views.add_scan, name='add_scan'),
+    url(r'^track/', views.add_character, name='add_character'),
 ]
