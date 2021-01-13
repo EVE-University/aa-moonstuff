fc9c01578c43d6f21a605e513162a0cbcaf0f9c4
colcrunch <rhartnett35@gmail.com>
2021-01-13 16:22:06 -0500

    Add list of ESI scopes needed for tracking character to constant tuple in providers.

--- moonstuff/providers.py
+++ moonstuff/providers.py
@@ -2,4 +2,10 @@ from esi.clients import EsiClientProvider
 
 from . import __version__
 
+ESI_CHARACTER_SCOPES = (
+    'esi-industry.read_corporation_mining.v1',
+    'esi-universe.read_structures.v1',
+    'esi-characters.read_notifications.v1',
+    )
+
 esi = EsiClientProvider(app_info_text="moonstuff v" + __version__)
