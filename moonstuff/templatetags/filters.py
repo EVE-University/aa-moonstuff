2f54b61d1c73fde9b8291a927a9cc51b096e8ef1
colcrunch <rhartnett35@gmail.com>
2021-01-18 23:22:56 -0500

    Add card baded display for upcoming extractions. (Still WIP)

--- moonstuff/templatetags/filters.py
+++ moonstuff/templatetags/filters.py
@@ -1,6 +1,6 @@
 from django.template.defaulttags import register
 from django.utils import timezone
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 @register.filter()
@@ -22,3 +22,53 @@ def get_next_extraction(moon):
     if ext_arrival > now:
         return datetime.strftime(exts[-1].arrival_time, '%Y-%m-%d %H:%M')
     return ''
+
+
+@register.filter()
+def card_labels(resources):
+    rare_values = set([r.rarity for r in resources])
+    return sorted(list(rare_values), reverse=True)
+
+
+@register.filter()
+def chunk_time(extraction):
+    """
+    Returns the number of days that the extraction will take.
+    :param extraction: Extraction model object.
+    :return:
+    """
+    start = extraction.start_time
+    end = extraction.arrival_time
+
+    delta = end - start
+    return delta.days
+
+
+@register.filter()
+def destruct_time(extraction):
+    """
+    Returns the self-destruct time for the extraction.
+    :param extraction: Extraction model object.
+    :return:
+    """
+    return extraction.arrival_time+timedelta(hours=3)
+
+
+@register.filter()
+def percent(quantity: float):
+    """
+    Converts decimal to percent.
+    :param quantity: float
+    :return:
+    """
+    return round(quantity * 100, 1)
+
+
+@register.filter()
+def order_quantity(resources):
+    """
+    Returns a list of resources ordered by quantity.
+    :param resources: QS containing the resources.
+    :return:
+    """
+    return sorted(list(resources), key=lambda r: r.quantity, reverse=True)
