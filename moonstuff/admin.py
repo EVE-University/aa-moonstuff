71fa1cb055d46c33f0080e5c08387a8326a952b7
colcrunch <rhartnett35@gmail.com>
2021-06-16 19:13:19 -0400

    Add TrackingCharacter to Admin

--- moonstuff/admin.py
+++ moonstuff/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from .models import Extraction, Refinery, TrackingCharacter
+
+
 # Register your models here.
+class TrackingCharacterAdmin(admin.ModelAdmin):
+    list_display = ("character",
+                    "get_corp",
+                    "latest_notification_id",
+                    "last_notification_check")
+
+    def get_corp(self, obj):
+        return obj.character.corporation_name
+
+    get_corp.short_description = "Corporation"
+    get_corp.admin_order_field = "character__corporation_name"
+
+    list_filter = ("character__corporation_name",)
+    search_fields = ("character__corporation_name", "character__character_name")
+
+
+admin.site.register(TrackingCharacter, TrackingCharacterAdmin)
