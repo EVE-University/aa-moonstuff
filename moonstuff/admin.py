7e6b3fcd3ccf824218390d42356cfb6e58f5068d
colcrunch <rhartnett35@gmail.com>
2021-06-16 22:19:12 -0400

    Add Resource model to admin.

--- moonstuff/admin.py
+++ moonstuff/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Extraction, Refinery, TrackingCharacter
+from .models import Extraction, Refinery, TrackingCharacter, Resource
 
 
 # Register your models here.
@@ -64,6 +64,42 @@ class RefineryAdmin(admin.ModelAdmin):
     search_fields = ("corp__corporation_name", "evetype__name", "name")
 
 
+class EveMoonFilter(admin.SimpleListFilter):
+    title = "Moon"
+    parameter_name = "moon"
+
+    def lookups(self, request, model_admin):
+        moons = set([r.moon for r in model_admin.model.objects.all()])
+        return [(m.id, m.name) for m in moons]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(moon__id__exact=self.value())
+
+
+class OreFilter(admin.SimpleListFilter):
+    title = "Ore"
+    parameter_name = "ore"
+
+    def lookups(self, request, model_admin):
+        ore = set([r.ore for r in model_admin.model.objects.all()])
+        return [(o.id, o.name) for o in ore]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(ore__id__exact=self.value())
+
+
+class ResourceAdmin(admin.ModelAdmin):
+    list_select_related = True
+    list_display = ("moon",
+                    "ore",
+                    "quantity")
+    list_filter = (EveMoonFilter, OreFilter)
+    search_fields = ("moon", "ore")
+
+
+admin.site.register(Resource, ResourceAdmin)
 admin.site.register(Refinery, RefineryAdmin)
 admin.site.register(Extraction, ExtractionAdmin)
 admin.site.register(TrackingCharacter, TrackingCharacterAdmin)
